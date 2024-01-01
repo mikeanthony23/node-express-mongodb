160cdddc5e3cb3dc7e6dd4612206418d9fce55cc
Mike Anthony Ygona <mikeyg@proweaver.net>
2024-01-01 18:21:18 +0800

    added factory function (createOne)

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -9,3 +9,31 @@ exports.deleteOne = Model =>
     }
     res.status(204).json({ status: 'success', data: { doc: null } });
   });
+
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+    res.status(201).json({
+      status: 'sucess',
+      data: {
+        data: doc,
+      },
+    });
+  });
