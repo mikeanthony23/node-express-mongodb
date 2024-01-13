c96c161b47874bb2a213c95f4b54b8a770a60643
Mike Anthony Ygoña <mikeanthony595@gmail.com>
2024-01-14 00:30:13 +0800

    updated bookings api

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('../controllers/handlerFactory');
 const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = () => {};
@@ -12,7 +14,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
@@ -39,3 +43,17 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session,
   });
 });
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, UNSECURE: anyone can make booking w/o payment
+  const { tour, user, price } = req.query;
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+  res.redirect(req.originalUrl.split('?')[0]);
+});
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBooking = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview,
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get(
@@ -13,6 +19,13 @@ router.get(
   authController.protect,
   viewsController.getAccount,
 );
+router.get(
+  '/my-tours',
+  authController.isLoggedIn,
+  authController.protect,
+  viewsController.getMyTour,
+);
+
 router.post(
   '/submit-user-data',
   authController.protect,
