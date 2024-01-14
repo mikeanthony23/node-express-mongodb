40903aecd4ef1a71cddce1fdc22ee40b4a6786cf
Mike Anthony Ygoña <mikeanthony595@gmail.com>
2024-01-14 20:31:24 +0800

    debugging stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -63,7 +63,7 @@ const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   // const price = 490 / 100;
-  const price = session.amount_total;
+  const price = session.amount_total / 100;
   // const tour = session.client_reference_id;
   // const user = (await User.findOne({ email: session.customer_email })).id;
   // const price = session.line_items[0].price_data.unit_amount / 100;
