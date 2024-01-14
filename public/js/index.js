1416fa2694cf907b025db225f4217ffe6c7b989d
Mike Anthony Ygoña <mikeanthony595@gmail.com>
2024-01-14 21:54:03 +0800

    added alert message on successfull booking

--- public/js/index.js
+++ public/js/index.js
@@ -66,4 +66,4 @@ if (bookBtn) {
 }
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alert) showAlert('success', alertMessage);
+if (alertMessage) showAlert('success', alertMessage);
